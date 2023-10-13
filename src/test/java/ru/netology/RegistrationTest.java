d0989f83e2174b3e9c5a5099031ab603f056c28e
Venajan <pokrovskiyalex77@gmail.com>
2023-10-13 19:29:42 +0300

    Changes

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -1,9 +1,7 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
